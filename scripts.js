fe5607c31ac742649d1a2b02f30b5ddd83fe1c6c
kingWahley <kingWahley2@gmail.com>
2024-08-05 21:27:13 +0100

    Your commit message

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'javiereduardop24@gmail.com' && password === 'vega') {
+    if (username === 'javiereduardop24@gmail.com' && password === '8rt788tgtfhbwycg89yfhiows849w') {
         window.location.href = 'dashboard.html';
     } else {
         alert('Invalid username or password');
